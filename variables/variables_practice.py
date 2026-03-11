5a0d76831640dde7ee77b5b4e5e82128ce9b022b
th3gr33nv1p3r <mohamed.e.koko@gmail.com>
2026-03-11 09:20:54 -0400

    another update

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -49,4 +49,4 @@ remaining_money = monthly_income - total_expenses
 # print(f"Expenses: ${total_expenses}")
 # print(f"Remaining Money: ${remaining_money}")
 
-# Exercise 5 - Temperature Converter
+# Exercise 5 - Temperature Converter dfdffgfgfgfgfgfgfdfdffdferttuh
